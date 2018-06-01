9581174ac5c11967d933546a66937a8e35675b60
Roberto Cervantes <robece@protonmail.com>
2018-06-01 15:06:17 -0500

    Updating welcome and legal concerns page

--- source/mobile-app/TuVotoCuenta/Pages/Legal/LegalConcernsPage.xaml.cs
+++ source/mobile-app/TuVotoCuenta/Pages/Legal/LegalConcernsPage.xaml.cs
@@ -1,28 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using TuVotoCuenta.Domain;
+using TuVotoCuenta.Interfaces;
 using TuVotoCuenta.ViewModels;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace TuVotoCuenta.Pages
 {
-    public partial class LegalConcernsPage : ContentPage
+	public partial class LegalConcernsPage : ContentPage
 	{
-        public LegalConcernsPage()
-        {
-            InitializeComponent();
+		public LegalConcernsPage()
+		{
+			InitializeComponent();
+
+            Xamarin.Forms.NavigationPage.SetHasNavigationBar(this, true);
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+
 			BindingContext = new LegalConcernsViewModel(this.Navigation);
-        }
+		}
 
 		public LegalConcernsPage(SignUpAccountRequest model)
-        {
-            InitializeComponent();
-			BindingContext = new LegalConcernsViewModel(this.Navigation, model);
-        }
+		{
+			InitializeComponent();
 
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();         
-        }
-    }
+			Xamarin.Forms.NavigationPage.SetHasNavigationBar(this, false);
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+
+			BindingContext = new LegalConcernsViewModel(this.Navigation, model);
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			Task.Run(async () =>
+			{
+				var legalConcerns = await DependencyService.Get<ILegalConcerns>().ReadLegalConcerns();
+
+				Device.BeginInvokeOnMainThread(() => { 
+					var htmlSource = new HtmlWebViewSource();
+					htmlSource.Html = legalConcerns;
+                    browser.Source = htmlSource;
+				});            
+			});
+		}
+	}
 }
