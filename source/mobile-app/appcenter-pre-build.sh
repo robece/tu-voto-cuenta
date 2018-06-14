08592ac9ba82fab3ea2d1aee6eafb1cba74736a7
Humberto Jaimes <humberto.jaimes@outlook.com>
2018-06-13 22:00:21 -0500

    Prebuilt Script Update

--- source/mobile-app/appcenter-pre-build.sh
+++ source/mobile-app/appcenter-pre-build.sh
@@ -7,11 +7,11 @@ echo " - STORAGE_URL: $STORAGE_URL"
 
 IdFile=$BUILD_REPOSITORY_LOCALPATH/source/mobile-app/TuVotoCuenta/SettingsInitializer.cs
 
-sed -i '' "s/CRYPTO_KEY/$CRYPTO_KEY/g" $IdFile
-sed -i '' "s/FUNCTION_URL/$FUNCTION_URL/g" $IdFile
-sed -i '' "s/STORAGE_URL/$STORAGE_URL/g" $IdFile
-sed -i '' "s/ACCOUNT_IMAGE_CONTAINER/$ACCOUNT_IMAGE_CONTAINER/g" $IdFile
-sed -i '' "s/RECORDS_IMAGE_CONTAINER/$RECORDS_IMAGE_CONTAINER/g" $IdFile
+sed -i '' "s#CRYPTO_KEY#$CRYPTO_KEY#g" $IdFile
+sed -i '' "s#FUNCTION_URL#$FUNCTION_URL#g" $IdFile
+sed -i '' "s#STORAGE_URL#$STORAGE_URL#g" $IdFile
+sed -i '' "s#ACCOUNT_IMAGE_CONTAINER#$ACCOUNT_IMAGE_CONTAINER#g" $IdFile
+sed -i '' "s#RECORDS_IMAGE_CONTAINER#$RECORDS_IMAGE_CONTAINER#g" $IdFile
 
 # Print out file for reference
 cat $IdFile
