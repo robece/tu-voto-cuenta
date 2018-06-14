6fa0dd5d1b80325b698cebdd3f05775321e9ace7
Humberto Jaimes <humberto.jaimes@outlook.com>
2018-06-13 21:46:41 -0500

    Prebuild Script update

--- source/mobile-app/appcenter-pre-build.sh
+++ source/mobile-app/appcenter-pre-build.sh
@@ -5,7 +5,7 @@ echo " - STORAGE_URL: $STORAGE_URL"
 
 # Updating ids
 
-IdFile=$BUILD_REPOSITORY_LOCALPATH/tu-voto-cuenta/source/mobile-app/TuVotoCuenta/SettingsInitializer.cs
+IdFile=$BUILD_REPOSITORY_LOCALPATH/source/mobile-app/TuVotoCuenta/SettingsInitializer.cs
 
 sed -i '' "s/CRYPTO_KEY/$CRYPTO_KEY/g" $IdFile
 sed -i '' "s/FUNCTION_URL/$FUNCTION_URL/g" $IdFile
